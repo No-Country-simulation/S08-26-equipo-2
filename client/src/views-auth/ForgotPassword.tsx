506abfed548455d2dd45bb69cd7b0d4f30a78897
Leslie Garcia <ggarciaveka@gmail.com>
2026-09-12 22:58:33 -0500

    Form forgot password

--- client/src/views-auth/ForgotPassword.tsx
+++ client/src/views-auth/ForgotPassword.tsx
@@ -1,8 +1,20 @@
-export default function ForgotPassword(){
-    return (
-        <div>
-            <h1>Recuperar contraseña</h1>
-            
+import { ForgotPasswordForm } from "@/components/forms/ForgotPasswordForm";
+import Spacing from "@/components/Spacing";
+import { Video } from "lucide-react";
+
+export default function ForgotPassword() {
+  //verificar que el email exista en DB
+  //enviar email para reset pw
+  return (
+    <div className="flex flex-col min-h-full justify-center items-center">
+      <div className="flex justify-center items-center gap-3">
+        <div className="flex size-9 items-center justify-center rounded-xl bg-blue-600">
+          <Video className="size-5 text-white" />
         </div>
-    )
+        <h1 className="font-extrabold text-3xl">Meetflow</h1>
+      </div>
+      <Spacing/>
+      <ForgotPasswordForm />
+    </div>
+  );
 }
